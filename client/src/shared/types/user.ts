e956d4382e430091f17435a26c4644f87cfb6b5f
rodry <rodrymza@gmail.com>
2026-06-08 23:25:51 -0300

    feat: types de user

---
+++ client/src/shared/types/user.ts
@@ -0,0 +1,30 @@
+export interface User {
+  id: number;
+  name: string;
+  lastname: string;
+  email: string;
+  role: string;
+  is_active: boolean;
+}
+
+export interface UserAdmin extends User {
+  created_at: string;
+  updated_at: string | null;
+}
+
+export interface UserPaginated {
+  data: UserAdmin[];
+  total: number;
+}
+
+export interface CreateUserPayload {
+  name: string;
+  lastname: string;
+  email: string;
+  password: string;
+  role: "ADMIN" | "OPERATOR";
+}
+
+export interface UpdateRolePayload {
+  role: "ADMIN" | "OPERATOR";
+}
